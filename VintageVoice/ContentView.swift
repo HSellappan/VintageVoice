ab1f52baa3d306fc32f4d395e6753b39471666dc
HSellappan <hasellan@gmail.com>
2025-10-22 21:44:18 -0500

    Initial Commit

---
+++ VintageVoice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VintageVoice
+//
+//  Created by Harold S on 10/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VintageVoice/VintageVoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  VintageVoiceApp.swift
+//  VintageVoice
+//
+//  Created by Harold S on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct VintageVoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
